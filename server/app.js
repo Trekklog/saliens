5142ff940a6d6d0f204ec16ca13442fb7ab8d01b
Trekklog <trekklog@gmail.com>
2018-08-21 06:53:26 -0600

    unnecessary files removed

--- server/app.js
+++ server/app.js
@@ -6,8 +6,6 @@ const logger = require('morgan');
 
 const cors = require('cors')
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const user_inventory = require('./routes/user_inventory')
 
 // mongodb / mongoose database
@@ -19,7 +17,6 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,9 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors())
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/inv', user_inventory)
 
 // catch 404 and forward to error handler
